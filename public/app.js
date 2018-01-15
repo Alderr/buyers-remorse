b0a1d737fc26d8cfca55b80a2072d9977dcaafbf
John Castillo <JohnluisCastillo@gmail.com>
2018-01-15 16:42:39 -0500

    Got barebones working

--- public/app.js
+++ public/app.js
@@ -1,32 +1,55 @@
 const TOP_LEVEL_COMPONENTS = [
-    '.begin','.results'];
+    '.begin', '.results'
+];
 
 const coins = {
-    bitcoin: ['8 Years Ago','4 Years ago','Last Year'],
-    bitcoinCash:['5 Months ago','2 Months ago','Yesterday'],
-    Ethereum:['2 Years ago', '1 Year ago', '6 Months ago'],
+    bitcoin: ['8 Years ago', '4 Years ago', 'Last Year'],
+    bitcoinCash: ['5 Months ago', '2 Months ago', 'Yesterday'],
+    Ethereum: ['2 Years ago', '1 Year ago', '6 Months ago'],
     Ripple: ['5 Years ago', '3 Years ago', '1 Year ago']
 }
-  
-  function hideAll() {
-    TOP_LEVEL_COMPONENTS.forEach(component => $(`${component}`).hide());
-  };
-
-//   function pushDropdown(response) {
-//     coins.forEach(function(ans){
-        
-//         });
-//     }
-
 
 let store = {
-    page: 'begin',
-    feedback: null
+    page: true,
+    feedback: null,
+    currentCoin: null
 };
 
+function hideAll() {
+    TOP_LEVEL_COMPONENTS.forEach(component =>
+        $(`${component}`).hide());
+};
 
-if(store.page === 'intro'){
-    $('.begin').show();
-}else if(store.page === 'answer'){
-    $('.results').show();
+function render() {
+    hideAll();
+    if (store.page === true) {
+        $('.begin').show();
+        $('.results').hide();
+
+    } else if (store.page === false) {
+        $('.results').show();
+        $('.begin').hide();
+    }
 }
+render();
+
+$('#coin').click(function(event) {
+    if (event.target.value === 'btc') {
+        store.currentCoin = coins.bitcoin
+    } else if (event.target.value === 'bch') {
+        store.currentCoin = coins.bitcoinCash
+    } else if (event.target.value === 'eth') {
+        store.currentCoin = coins.Ethereum
+    } else if (event.target.value === 'xrp') {
+        store.currentCoin = coins.Ripple
+    }
+    $('#select').html(`<option value="option1">${store.currentCoin[0]}</option>
+    <option value="option2">${store.currentCoin[1]}</option>
+    <option value="option3">${store.currentCoin[2]}</option>`);
+
+});
+
+$('.submit').click(() => {
+  store.page = !store.page;
+  render();
+})
